3d6e19ca18197f4a6bed8a5e9abc7c1c4621a523
Raynald de Lahondes <raynald@delahondes.com>
2024-11-29 08:14:41 +0100

    Add loop to compute 100000 individual to test speed

--- src/main.rs
+++ src/main.rs
@@ -7,21 +7,26 @@ use individuals::Individual;
 
 fn main() {
     let mut my_data = Data::new();
-    my_data.X = vec! [ vec! [0.1,0.2,0.3], vec! [0.0, 0.0, 0.0], vec! [0.9,0.8,0.7] ];
-    my_data.samples = string_vec! ["a","b","c"];
-    my_data.features = string_vec! ["msp1","msp2","msp3"];
-    my_data.y = vec! [0,1,1];
-    //my_data.load_data("samples/X.tsv","samples/y.tsv");
+    //my_data.X = vec! [ vec! [0.1,0.2,0.3], vec! [0.0, 0.0, 0.0], vec! [0.9,0.8,0.7] ];
+    //my_data.samples = string_vec! ["a","b","c"];
+    //my_data.features = string_vec! ["msp1","msp2","msp3"];
+    //my_data.y = vec! [0,1,1];
+    my_data.load_data("samples/X.tsv","samples/y.tsv");
     println!("{:?}", my_data); // Example usage
     
-    //let my_individual = Individual::random(&my_data);
-    let mut my_individual = Individual::new();
-    my_individual.features = vec! [1,0,-1];
+    let mut auc_max = 0.0;
+    let mut best_model: Vec<u8>;
+    let mut best_individual: Individual = Individual::new();
+    for i in 0..100000 {
+    let my_individual = Individual::random(&my_data);
+    //let mut my_individual = Individual::new();
+    //my_individual.features = vec! [1,0,-1];
 
-    println!("my individual: {:?}",my_individual.features);
-    println!("my individual evaluation: {:?}",my_individual.evaluate(&my_data));
-    println!("my individual auc: {:?}",my_individual.compute_auc(&my_data));
-    
-
+    //println!("my individual: {:?}",my_individual.features);
+    //println!("my individual evaluation: {:?}",my_individual.evaluate(&my_data));
+    let auc = my_individual.compute_auc(&my_data);
+    if (auc>auc_max) {auc_max=auc;best_individual=my_individual;}
+    }
+    println!("auc max: {} model: {:?}",auc_max, best_individual.features);
 }
 
