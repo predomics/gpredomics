1c45b80c9c5beadc148458c4890e09cfa27e55d4
Raynald de Lahondes <raynald@delahondes.com>
2025-01-18 17:32:34 +0100

    Fails with arm64 symbol missing (but at list building seems to work properly with cmake crate)

--- build.rs
+++ build.rs
@@ -5,6 +5,6 @@ fn main() {
     // Run the CMake build process
     let dst = Config::new("libkokkos_bridge").build();
 
-    println!("cargo:rustc-link-search=native={}/lib", dst.display());
+    println!("cargo:rustc-link-search=native={}/build/out/lib", dst.display());
     println!("cargo:rustc-link-lib=static=kokkos_bridge");
 }
