cea1928fcafcc06050f10af0647b791b0e810394
Raynald de Lahondes <raynald@delahondes.com>
2025-01-20 08:06:56 +0100

    trying to understand linking errors

--- build.rs
+++ build.rs
@@ -5,6 +5,7 @@ fn main() {
     // Run the CMake build process
     let dst = Config::new("libkokkos_bridge").build();
 
+    //println!("cargo:rustc-link-arg=-mmacosx-version-min=11.0");
     println!("cargo:rustc-link-search=native={}/build/out/lib", dst.display());
     println!("cargo:rustc-link-lib=static=kokkos_bridge");
 }
